640bf160af923e394130f5d9e3a059b394c90b2c
renato lemos <renatolemos83@gmail.com>
2016-10-27 23:08:01 -0200

    Revert "Teste Supero"
    This reverts commit 119512dc2cd880653caf11558e0e46d7ff7ee840.

--- restrito.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();  //A seção deve ser iniciada em todas as páginas
-if (isset($_SESSION['id'])) {  //Verifica se há seções
-    session_destroy();      //Destroi a seção por segurança
-    header("Location: lista.php");
-    exit; //Redireciona o visitante para login
-}
-?>
--- sair.php
+++
@@ -1,7 +0,0 @@
-<?php
-//include 'restrito.php';
-session_start();
-session_destroy();
-header("Location: index.php");
-exit;
-?>
